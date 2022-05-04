5b8677395bed99814646fe4609d84367b77af1f6
pskalskii <pawelskalski97@gmail.com>
2022-05-04 21:03:03 +0200

    opis-js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let paragraph = "p";
+document.getElementById("opis");
+
+let firstButton = document.querySelector("button");
+firstButton.onclick = function () {
+  opis.textContent = "Losowy Napis";
+}
